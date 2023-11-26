4dc26e2e1465feb040a06b61228c5ed53cbfd815
Fiol Thiago <fiolthiago9@gmail.com>
2023-11-26 18:59:11 -0300

    agregamos la ultima funcionalidad para el punto 1

--- js/app.js
+++ js/app.js
@@ -1,13 +1,26 @@
 const botonComenzar = document.getElementById("Comenzar");
 const formUsu = document.getElementById("numeroUsuario");
-let numeroAleatorio = 0;
+let numeroAleatorio;
 function numAleatorio() {
   numeroAleatorio = Math.floor(Math.random() * 50) + 1;
   botonComenzar.className = "btn btn-danger disabled";
   formUsu.className = "my-3";
+  console.log(numeroAleatorio);
 }
 function numeroUsu(e) {
-    e.preventDefault();
+  e.preventDefault();
+  const inputBusqueda = document.querySelector("input");
+  const numeroIngresado = parseInt(inputBusqueda.value);
+  if (numeroIngresado === numeroAleatorio) {
+    alert("felicidades, el numero aleatorio fue " + numeroAleatorio);
+  } else {
+    if (numeroIngresado > numeroAleatorio) {
+      alert("el numero ingresado es mayor que el numero aleatorio");
+    } else {
+      alert("el numero ingresado es menor que el numero aleatorio");
+    }
+  }
+  formUsu.reset();
 }
 botonComenzar.addEventListener("click", numAleatorio);
 formUsu.addEventListener("submit", numeroUsu);
